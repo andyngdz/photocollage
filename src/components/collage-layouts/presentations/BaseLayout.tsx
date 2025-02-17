587815e1b8966bd80fd9ebd01e4097dbe5311d67
Andy D. Nguyen <10527554+andyngdz@users.noreply.github.com>
2025-02-17 22:20:57 +0700

    Update BaseLayout to accept a single className and apply grid-cols-2 in TwoImages

--- src/components/collage-layouts/presentations/BaseLayout.tsx
+++ src/components/collage-layouts/presentations/BaseLayout.tsx
@@ -2,7 +2,7 @@ import clsx, { ClassValue } from "clsx";
 import { PropsWithChildren, useMemo } from "react";
 
 export interface IBaseLayoutProps<T> extends PropsWithChildren {
-  className?: ClassValue[];
+  className?: ClassValue;
   items: T[];
   onRender: (item: T) => React.ReactNode;
 }
@@ -18,7 +18,6 @@ export const BaseLayout = <T,>({
     <div
       className={clsx(
         "grid overflow-hidden",
-        "grid-cols-2",
         "gap-1",
         "w-[inherit]",
         "h-[inherit]",
